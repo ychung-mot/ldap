67e298ac71cbcf8982fb8ea07ef5aced320d9be3
Young-Jin Chung <Young-Jin.Chung@gov.bc.ca>
2020-12-10 13:20:24 -0800

    .net 3.1

--- api/ldapmain/Startup.cs
+++ api/ldapmain/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Novell.Directory.Ldap;
 using System;
 using System.Collections.Generic;
@@ -30,10 +29,6 @@ namespace ldapmain
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ldapmain", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +37,6 @@ namespace ldapmain
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ldapmain v1"));
             }
 
             app.UseHttpsRedirection();
