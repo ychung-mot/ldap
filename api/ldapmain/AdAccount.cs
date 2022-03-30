f2bab9769595a94bde378814d79f23d24be1d2a4
ychung-mot <young-jin.chung@dxcas.com>
2022-03-30 12:32:09 -0700

    group

---
+++ api/ldapmain/AdAccount.cs
@@ -0,0 +1,21 @@
+﻿using Novell.Directory.Ldap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ldapmain
+{
+    public class AdAccount
+    {
+        public string Username { get; set; }
+        public Guid UserGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Company { get; set; }
+        public string Department { get; set; }
+        public List<string> MemberOf { get; set; }
+    }
+}
--- api/ldapmain/Startup.cs
+++ api/ldapmain/Startup.cs
@@ -1,16 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Novell.Directory.Ldap;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace ldapmain
 {
@@ -19,7 +19,7 @@ namespace ldapmain
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            LdapSearch();
+            LdapSearch("sAMAccountName", "dso");
         }
 
         public IConfiguration Configuration { get; }
@@ -27,7 +27,6 @@ namespace ldapmain
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
         }
 
@@ -51,27 +50,72 @@ namespace ldapmain
             });
         }
 
-        private void LdapSearch()
+        private AdAccount LdapSearch(string filterAttr, string value)
         {
             var server = Configuration["LdapServer"];
             var account = Configuration["Account"];
             var password = Configuration["Password"];
             var ldapPort = Convert.ToInt32(Configuration["LdapPort"]);
 
-            Console.WriteLine(@$"IDIR\{account}-{password}");
-
             using var conn = new LdapConnection() { SecureSocketLayer = false };
             conn.Connect(server, ldapPort);
+            conn.UserDefinedServerCertValidationDelegate += Conn_UserDefinedServerCertValidationDelegate;
+            conn.StartTls();
             conn.Bind(@$"IDIR\{account}", password);
 
-            var filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=dso))";
-            var search = conn.Search("OU=BCGOV,DC=idir,DC=BCGOV", LdapConnection.ScopeSub, filter, new string[] { "bcgovGUID", "sn", "givenname" }, false);
+            var filter = $"(&(objectCategory=person)(objectClass=user)({filterAttr}={value}))";
+            var search = conn.Search("OU=BCGOV,DC=idir,DC=BCGOV", LdapConnection.ScopeSub, filter, new string[] { "sAMAccountName", "bcgovGUID", "givenName", "sn", "mail", "displayName", "company", "department", "memberOf" }, false);
 
             var entry = search.FirstOrDefault();
 
-            Console.WriteLine(entry.GetAttribute("bcgovGUID").StringValue);
-            Console.WriteLine(entry.GetAttribute("sn").StringValue);
-            Console.WriteLine(entry.GetAttribute("givenname").StringValue);
+            if (entry == null)
+                return null;
+
+            var adAccount = new AdAccount
+            {
+                Username = GetAttributeStringValue(entry, "sAMAccountName"),
+                UserGuid = new Guid(GetAttributeStringValue(entry, "bcgovGUID")),
+                FirstName = GetAttributeStringValue(entry, "givenName"),
+                LastName = GetAttributeStringValue(entry, "sn"),
+                Email = GetAttributeStringValue(entry, "mail"),
+                DisplayName = GetAttributeStringValue(entry, "displayName"),
+                Company = GetAttributeStringValue(entry, "company"),
+                Department = GetAttributeStringValue(entry, "department"),
+                MemberOf = GetAttributeObjValue(entry, "memberOf"),
+            };
+
+            Console.WriteLine(JsonSerializer.Serialize(adAccount));
+
+            return adAccount;
+        }
+
+        private bool Conn_UserDefinedServerCertValidationDelegate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            if (chain.ChainElements == null)
+                return false;
+
+            return true;
+        }
+        private string GetAttributeStringValue(LdapEntry entry, string attribute)
+        {
+            return entry.GetAttributeSet().Any(x => x.Key == attribute) ? entry.GetAttribute(attribute).StringValue : "";
+        }
+
+        private List<string> GetAttributeObjValue(LdapEntry entry, string attribute)
+        {
+            var values = entry.GetAttributeSet().Any(x => x.Key == attribute) ? entry.GetAttribute(attribute).StringValueArray : Array.Empty<string>();
+
+            var attValues = new List<string>();
+
+            foreach(var value in values)
+            {
+                attValues.Add(Regex.Match(value, "^CN=(.+?),").Groups[1].Value);
+            }
+
+            return attValues;
         }
     }
 }
